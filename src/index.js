d1dc5dfae41512f2f215638b625962c0377b7077
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-09-04 09:32:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -57,7 +58,9 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+
+  return <h1 style={style}>Fast React Pizza Co.</h1>;
 }
 
 function Menu() {
