bf9a994fe79ed88e5a3468bc444db4be7d4f41c8
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-09-09 23:30:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -70,15 +70,20 @@ function Header() {
 
 function Menu() {
   const pizzas = pizzaData;
+
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObject={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObject={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
